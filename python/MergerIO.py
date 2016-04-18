f4b327eb5712ffe5b20d4c4fe0d7ad04a120514f
matthias <matthias.luethi@lhep.unibe.ch>
2016-04-18 16:34:06 +0200

    correct written root file - again.

--- python/MergerIO.py
+++ python/MergerIO.py
@@ -48,5 +48,5 @@ class MergerIO(object):
 
     def WriteMap(self, array):
         self.log.info("writing map to " + self.OutFile)
-        arr = np.array(array,dtype=[ ("map", np.uint)])
+        arr = np.array(array,dtype=[ ("map", np.uint32)])
         array2root(arr, self.OutFile, mode='recreate')
