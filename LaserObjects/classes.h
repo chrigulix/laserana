e0598e2bb0ce6e3c7392955296337219b2ab5dd2
maluethi <matthias.luethi@lhep.unibe.ch>
2016-04-26 19:02:09 +0200

    added fucking generation of fucking dictionary

---
+++ LaserObjects/classes.h
@@ -0,0 +1,21 @@
+//
+// Build a dictionary.
+//
+
+// Original author Rob Kutschke
+//
+
+#include "LaserBeam.h"
+#include "art/Persistency/Common/Wrapper.h"
+//
+// Only include objects that we would like to be able to put into the event.
+// Do not include the objects they contain internally.
+//
+
+namespace {
+    struct dictionary {
+        lasercal::Time tm;
+        lasercal::LaserBeam la;
+        art::Wrapper<lasercal::LaserBeam> laser;
+    };
+}
