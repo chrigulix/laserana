f2dd0cccacae1b7e60d3d1918a901081045b63c4
matthias <matthias.luethi@lhep.unibe.ch>
2016-05-23 13:57:17 +0200

    viewer for cropped views

--- python/PowerAna.py
+++ python/PowerAna.py
@@ -7,11 +7,15 @@ from datadefs.lar_data import *
 
 def crop_view(limits, hits):
     cols = []
-    for idx in range(len(hits[1, :])):
-        if limits[0] < hits[1, idx] < limits[1]:
+    for idx in range(len(hits.tick)):
+        if not limits[0] < hits.tick[idx] < limits[1]:
             cols.append(idx)
 
-    return hits[:, cols]
+    # apply the cut on all elements in the hits
+    for element in hits.dtype.names:
+        hits[element] = np.delete(hits[element], cols)
+
+    return hits
 
 
 filename = "/mnt/lheppc46/data/larsoft/userdev/maluethi/laser/test/outs.root"
@@ -25,11 +29,15 @@ laser_tick_offset = 100
 laser_tick_limits = [laser_tick_mean - laser_tick_offset, laser_tick_mean + laser_tick_offset]
 
 for i in range(data.n_entries):
+    print "Event: ", i
+    laser = data.get_laser(i)
+    print laser.power
+
     hit = crop_view(laser_tick_limits, data.get_hits(i))
 
     fig, axs = plt.subplots(nrows=1, ncols=1, sharex=True)
     ax = axs
-    ax.errorbar(hit[0, :], hit[1, :], yerr=[hit[1, :] - hit[3, :], hit[4, :] - hit[1, :]], fmt='o')
+    ax.errorbar(hit.channel, hit.tick, yerr=[hit.tick - hit.start_tick, hit.end_tick - hit.tick], fmt='o')
+    #ax.errorbar(hit.channel, hit.tick, yerr=1.0, fmt='o')
     plt.show()
-
 
