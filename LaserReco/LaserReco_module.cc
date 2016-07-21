9ec39d130c1b5b1a32c7161c2af371e2e3531ee2
matthias <matthias.luethi@lhep.unibe.ch>
2016-07-21 15:36:46 +0200

    Made LaserHit finder use the LaserHitBox size specified in the fhicl file instead of the hard coded 10cm.

--- LaserReco/LaserReco_module.cc
+++ LaserReco/LaserReco_module.cc
@@ -178,6 +178,7 @@ namespace LaserReco {
       // Switches
       fParameterSet.WireMapGenerator = parameterSet.get<bool>("GenerateWireMap");
       fParameterSet.LaserROIFlag = parameterSet.get<bool>("GenerateWireMap");
+      fParameterSet.HitBoxSize = parameterSet.get<float>("HitBoxSize");
       
       // Tag for reading raw digit data
       fParameterSet.RawDigitTag = parameterSet.get<art::InputTag>("LaserRecoModuleLabel");
