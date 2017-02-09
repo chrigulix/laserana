0997f29eac3d026a79a264af801c5d30341e6193
matthias <matthias.luethi@lhep.unibe.ch>
2017-02-09 14:45:40 +0100

    new script to plot saved tracks

--- python/utils/plot_tracks.py
+++ python/utils/plot_tracks.py
@@ -1,38 +1,24 @@
-import root_numpy as rn
 import numpy as np
 import matplotlib.pyplot as plt
 
-filename = "~/workspace/laser/runs/tracks/Tracks-7205-790.root"
-tree = 'TimeMapProducer/Tracks'
-
-data = rn.root2array(filename, treename=tree)
-
-laser = np.array([1036, 109])
-in_range = 25
+filename = "laser_tracks.npy"
+tracks = np.load(filename)
 
 
-for entry in range(len(data)):
-	print entry, data[entry][4]
-	x, y, z = data[entry][1], data[entry][2], data[entry][3]
-	
-	m = abs((x[-1] - x[0]) / (z[-1] - z[0]))
-	
-	print m
-	
-	closest = np.argmax(z)
-	closest_point = np.array([z[closest], x[closest]])
-	
-	in_region = 50 > np.sqrt(np.sum(np.power(closest_point - laser,2)))
-	print in_region	
+fontsize=18
 
-	if m < 0.2 and len(z) > 1500 and in_region:
-		plt.subplot(211)
-		plt.scatter(z,x)
-		plt.xlim([0,1100])
-		plt.ylim([0,256])
-		
-		plt.subplot(212)
-		plt.scatter(z,y)
-		plt.xlim([0,1100])
-		plt.ylim([-128,128])
+plt.figure(figsize=(16,6), dpi=80)
+
+for track in tracks:
+    x, z = track[1], track[3]
+    plt.scatter(z, x, alpha=0.3, s=4)
+
+plt.xlim([0, 1035])
+plt.ylim([0, 250])
+
+plt.xlabel("z [cm]", fontsize=fontsize)
+plt.ylabel("x (drift) [cm]", fontsize=fontsize)
+plt.xticks(fontsize=fontsize)
+plt.yticks(fontsize=fontsize)
+plt.grid()
 plt.show()
