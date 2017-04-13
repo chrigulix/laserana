29745588eee3304a0684a9d05e28cf37b92ba8be
matthias <matthias.luethi@lhep.unibe.ch>
2017-04-13 09:20:44 +0200

    some cosmetics and added function to plot lines and get_branches

--- python/utils/lar_utils.py
+++ python/utils/lar_utils.py
@@ -1,6 +1,7 @@
 import root_numpy as rn
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
 
 import types
 
@@ -9,18 +10,29 @@ import matplotlib.cm as cm
 colors = iter(cm.rainbow(np.linspace(0, 1, 100)))
 
 # Plotting
-def plot_track(x, y, z, axes, color=None):
+def plot_track(x, y, z, axes, **kwargs):
 
     ax_zx, ax_zy, ax_xy = axes
 
+    color = None
+
     if color is None:
-        color = cm.viridis(1)
+        color = None #cm.viridis(1)
     else:
         color = next(colors)
 
-    ax_zx.scatter(z, x, c=color)
-    ax_zy.scatter(z, y, c=color)
-    ax_xy.scatter(x, y, c=color)
+    ax_zx.scatter(z, x, **kwargs)
+    ax_zy.scatter(z, y, **kwargs)
+    ax_xy.scatter(x, y, **kwargs)
+
+def plot_lines(lines, axes):
+    ''' this is plotting each line collection on the respective axes, so both arguments should have the
+     same size.'''
+    if lines is None:
+        return
+
+    for line_collection, ax in zip(lines, axes):
+        ax.add_collection(LineCollection(line_collection, linewidths=(0.5), linestyles='solid'))
 
 
 def make_figure():
@@ -32,12 +44,18 @@ def make_figure():
 
     ax_zx.set_xlim([0, 1100])
     ax_zx.set_ylim([0, 256])
+    ax_zx.set_xlabel("z [cm]")
+    ax_zx.set_ylabel("x [cm]")
 
     ax_zy.set_xlim([0, 1100])
     ax_zy.set_ylim([-128, 128])
+    ax_zy.set_xlabel("z [cm]")
+    ax_zy.set_ylabel("y [cm]")
 
     ax_xy.set_xlim([0, 256])
     ax_xy.set_ylim([-128, 128])
+    ax_xy.set_xlabel("x [cm]")
+    ax_xy.set_ylabel("y [cm]")
 
     axes = [ax_zx, ax_zy, ax_xy]
 
@@ -66,12 +84,33 @@ def calc_line(point1, point2):
 
 
 # reading
-def find_tree(argument, filename):
+def find_tree(tree_to_look_for, filename):
     ''' Find argument tree name that contains Track Information '''
     trees = rn.list_trees(filename)
     try:
-        track_tree = next(tree for tree in trees if argument in tree)
+        track_tree = next(tree for tree in trees if tree_to_look_for.lower() in tree.lower())
     except StopIteration:
-        raise ValueError("No track tree found in file")
+        raise ValueError("No tree with name \"" + tree_to_look_for + "\" found in file")
 
     return track_tree
+
+
+def get_branches(filename, treename, vectors=None):
+    ''' function that returns the appropriate branch string in a branch for the specified vectors of the branch  '''
+    if vectors is None:
+        return rn.list_branches(filename, treename=treename)
+
+    all_branches = rn.list_branches(filename, treename=treename)
+
+    for vector in vectors:
+        try:
+            all_branches.pop(all_branches.index(vector))
+        except:
+            continue
+
+        all_branches.extend([vector+'.x()', vector+'.y()', vector+'.z()'])
+
+    return all_branches
+
+
+
