ba679c2b9490196f1c2cc6f573866d49139ba9b7
matthias <matthias.luethi@lhep.unibe.ch>
2017-04-19 10:36:58 +0200

    added np.gradient as a second way of getting the slope.

--- python/utils/demonstrate_delata.py
+++ python/utils/demonstrate_delata.py
@@ -1,8 +1,6 @@
 import root_numpy as rn
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy import interpolate
-import sys
 
 from lar_utils import *
 
@@ -16,18 +14,25 @@ m_line, b_line = calc_line(line_p1, line_p2)
 
 # spline interpolation at each sampling point
 tck = interpolate.splrep(z, x)
-
 # get the slope of the tanget in each point
-slope = interpolate.splev(z, tck, der=1)
+slope_splines = interpolate.splev(z, tck, der=1)
 
-# get all the line parameters of the orthogonal
-m, b = calc_line_slope([z, x], -1 / slope)
+# same approach but now with np.gradient, where we have to cheat a little to get to the same length as the input vector
+# because the length of np.diff output is one smaller than the input vector.
+diff = np.diff(z)
+diff = np.append(diff, diff[-1])
+slope_gradient = np.gradient(x, diff, edge_order=2)
 
-# get the intersection points
-z_inter, x_inter = calc_intersect(m_line, b_line, m, b)
+for slope, layout in zip([slope_splines, slope_gradient], ['*-', 'o-']):
+    # get all the line parameters of the orthogonal
+    m, b = calc_line_slope([z, x], -1 / slope)
 
-# plot the line itself and the connection of the intersection point and the point on the line
-plt.plot([z_inter, z], [x_inter, x], '*-')
+    # get the intersection points
+    z_inter, x_inter = calc_intersect(m_line, b_line, m, b)
+
+    # plot the line itself and the connection of the intersection point and the point on the line
+    print layout
+    ax = plt.plot([z_inter, z], [x_inter, x], layout, alpha=0.3)
 
 plt.axes().set_aspect('equal', 'datalim')
 plt.show()
