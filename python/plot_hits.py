4d75d66ff75bd4698a620f1e64fe0d41ce513f1a
matthias <matthias.luethi@lhep.unibe.ch>
2017-05-03 09:41:12 +0200

    now all three planes are plotted seperately and are colorcoded with amplitude

--- python/plot_hits.py
+++ python/plot_hits.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 from datadefs.metadata import *
 
 import argparse
+import matplotlib.cm as cm
+import matplotlib as mpl
+
 
 def view(data, eventid):
 
@@ -12,19 +15,48 @@ def view(data, eventid):
     laser_tick_offset = 1200
 
     hit = data.get_hits(eventid)
+    event_id = data.ids[eventid]
     #if hit[1].size == 0:
     #    return
 
-    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(20,5))
+    fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(20,5))
 
-    ax.errorbar(hit.wire, hit.tick, yerr=[hit.tick - hit.start_tick, hit.end_tick - hit.tick], fmt='o')
+    for idx, ax in enumerate(axes):
+        plane_idx = np.where(hit.plane == idx)
+        errorbar = [hit.tick[plane_idx] - hit.start_tick[plane_idx],
+                     hit.end_tick[plane_idx] - hit.tick[plane_idx]]
+
+        ax.scatter(hit.wire[plane_idx],
+                         hit.tick[plane_idx],
+                         #yerr=50, #errorbar,
+                         #fmt='o',
+                         c=hit.peak_amp[plane_idx].astype(int),
+                         cmap=cm.viridis,
+                        norm=mpl.colors.Normalize(vmin=-1000, vmax=1000)
+
+                    )
+        err = ax.errorbar(hit.wire[plane_idx],
+                     hit.tick[plane_idx],
+                     yerr=errorbar,
+                          marker=None,
+                          fillstyle=None,
+                          fmt='o')
+
+
+        #plt.colorbar(ax=ax)
+
+    ax_ind0, ax_ind1, ax_col = axes
+
+    ax_ind0.get_shared_y_axes().join(ax_ind0, ax_ind1)
+    ax_ind1.get_shared_y_axes().join(ax_ind1, ax_col)
+
     plt.xlabel("Wire Number")
     plt.ylabel("Time Tick")
 
     #plt.xlim([0,3500])
     #plt.ylim([2000,10000])
 
-    plt.title("Event " + str(eventid))
+    ax_ind0.set_title("Event " + str(event_id))
     plt.show()
 
 
@@ -42,12 +74,12 @@ for file in args.files:
 
     data.get_info()
     data.read_ids()
-    data.read_hits(planes="y")
+    data.read_hits(planes="u")
 
 
 
     if args.events is None:
-        for i in range(0, data.n_entries):
+        for i in range(data.n_entries):
             view(data, i)
             print "Event " + str(data.ids[i])
     else:
