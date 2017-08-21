2a364d6ae45094f1765ee5effd3cc9d23d36b9f6
matthias <matthias.luethi@lhep.unibe.ch>
2017-08-21 10:15:01 +0200

    minor plotting adjustements

--- python/utils/plot_selected_tracks.py
+++ python/utils/plot_selected_tracks.py
@@ -5,14 +5,21 @@ from lar_utils import *
 
 
 
-tracks_filename = "data/laser-tracks-7205-test-nd.npy"
-laser_filename = "data/laser-data-7205-test-nd.npy"
+tracks_filename = "data/laser-tracks-7252-test-roi.npy"
+laser_filename = "data/laser-data-7252-test-roi.npy"
+
+#tracks_filename = "data/laser-tracks-7267-test-roi.npy"
+#laser_filename = "data/laser-data-7267-test-roi.npy"
+
+tracks_filename = 'data/laser-tracks-7267-test-rough.npy'
+laser_filename = 'data/laser-data-7267-test-rough-calib2.npy'
 
 tracks = np.load(tracks_filename)
 laser = np.load(laser_filename)
 
+plot_truth = True
 
-fig, axes = make_figure()
+fig, axes = make_figure(tpc_box=False)
 
 zx_laser_lines = []
 zy_laser_lines = []
@@ -25,20 +32,28 @@ laser_pos = np.array([115, 10, 1036])
 
 laser_distance = 32
 
-for laser, track in zip(laser, tracks):
+start = 18*100
+modulo = 100
+
+# this is just to save some time while plotting by reducing number of points in plot
+downsample = 1
+
+for laser, track in zip(laser[start::downsample], tracks[start::downsample]):
     x, y, z = track[1], track[2], track[3]
     laser_entry = np.rec.array([laser[1], laser[2], laser[3]],
-                               dtype = [('x', 'f'), ('y', 'f'), ('z', 'f')])
+                               dtype=[('x', 'f'), ('y', 'f'), ('z', 'f')])
     laser_exit = np.rec.array([laser[4],laser[5],laser[6]],
                               dtype=[('x', 'f'), ('y', 'f'), ('z', 'f')])
 
     event_id = track[0]
+    laser_id = laser[0]
     subrun = event_id / 50
 
     print "Event", event_id, "Subrun", subrun
+    print laser_id
 
     plotting_args = {"color": None, "s": 1}
-    plot_track(x,y,z,axes, **plotting_args)
+    plot_track(x, y, z, axes, **plotting_args)
 
     # plotting lines in a bulk: so we store them here for later
     zx_laser_lines.append([(laser_entry.z, laser_entry.x),(laser_exit.z, laser_exit.x)])
@@ -48,22 +63,21 @@ for laser, track in zip(laser, tracks):
     colors.append(int(laser_exit.y))
 
     # some module stuff, so you can plot few entries
-    modulo = 100
+
     if (idx + 1) % modulo == 0:
         start_lines = int((idx) / modulo) * modulo
         end_lines = start_lines + modulo
 
-        print start_lines, end_lines
-
-        plot_lines([zx_laser_lines[start_lines:end_lines],
-                    zy_laser_lines[start_lines:end_lines],
-                    xy_laser_lines[start_lines:end_lines]],
-                   axes,
-                   colors=colors)
+        if plot_truth:
+            plot_lines([zx_laser_lines[start_lines:end_lines],
+                        zy_laser_lines[start_lines:end_lines],
+                        xy_laser_lines[start_lines:end_lines]],
+                       axes)
         plt.show()
 
         fig, axes = make_figure()
     idx += 1
 
-plot_lines([zx_laser_lines, zy_laser_lines, xy_laser_lines], axes)
+if plot_truth:
+    plot_lines([zx_laser_lines, zy_laser_lines, xy_laser_lines], axes)
 plt.show()
