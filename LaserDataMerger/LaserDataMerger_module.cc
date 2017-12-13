970f8aeef0ee6d0e5a6cc1fdfe8d86a98ac66ff7
matthias <matthias.luethi@lhep.unibe.ch>
2017-12-13 12:51:59 +0100

    FIX: Trying to fix direction problems

--- LaserDataMerger/LaserDataMerger_module.cc
+++ LaserDataMerger/LaserDataMerger_module.cc
@@ -317,7 +317,7 @@ namespace LaserDataMerger {
             //Theta = TMath::DegToRad() * (LinearRawToAngle(Theta_raw) - 207.69487762388422);
             //Phi = -TMath::DegToRad() * (180 + (Phi_raw - fDirCalLCS2[0]));
 
-            Theta = TMath::DegToRad() * (297.3080594455559 - LinearRawToAngle(Theta_raw - fDirCalLCS2[1]));
+            Theta = TMath::DegToRad() * (LinearRawToAngle(Theta_raw - fDirCalLCS2[1]));
             Phi = -TMath::DegToRad() * (Phi_raw - fDirCalLCS2[0]);
             Position = PositionLCS2;
         } else {
